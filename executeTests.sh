4a3f802fe692be6477f00f99d236f5c465e30987
idjayant <idjayant@users.noreply.github.com>
2017-11-07 16:54:00 -0800

    Update executeTests.sh

--- executeTests.sh
+++ executeTests.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Executing go unit tests!
-echo Executing go unit tests!
+echo ********** Executing go unit tests! ***********
 go test
+
+echo ********** Tests execution done ***********
